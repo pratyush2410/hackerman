b5df57a2b538e3b8e889e935e2d05f62bd91287b
pratyush2410 <pratyush2410@gmail.com>
2020-03-15 17:10:41 +0530

    initial commit for dev branch

--- main.c
+++ main.c
@@ -4,5 +4,8 @@
 int main()
 {
     printf("Hello world!\n");
+    printf("this is my first git code");
+    printf("this is a change in my code");
+    printf("in dev branch now");
     return 0;
 }
